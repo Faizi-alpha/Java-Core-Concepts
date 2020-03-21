1bd010cf8cf4e831d228d36e938a8faa0c80e5c0
Faizan Ahmed Siddiqui <39314095+Faizi-alpha@users.noreply.github.com>
2020-03-21 14:43:07 +0530

    Update multiple_catch.java

--- multiple_catch.java
+++ multiple_catch.java
@@ -15,8 +15,8 @@ public class multiple_catch {
         catch(Exception e){
             System.out.print("Generic Exception Catch");
     }
-        /*this below exception is never reached because ArithmeticException is a subclass if Exception
-          catch(ArithmeticException e)     This is invalid because the exception has already been caught above.
+        /*this below exception is never reached because ArithmeticException is a subclass of Exception
+          catch(ArithmeticException e)    -->  This is invalid because the exception has already been caught above.
          {
             System.out.print("This is never reached");
          }
